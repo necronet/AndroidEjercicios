cdb4c4d6355274a58cee2e28a56feb97a069dce1
Jose Ayerdis <joseayerdis@gmail.com>
2011-10-13 17:18:53 -0600

    Ejercicio1 proporciona una aplicacion basica con capacidad de llamados a actividades en reversas

--- Ejercicio1/src/com/ejemplo/ListaRelacionActivity.java
+++ Ejercicio1/src/com/ejemplo/ListaRelacionActivity.java
@@ -3,11 +3,13 @@ package com.ejemplo;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class ListaRelacionActivity extends Activity {
 
+	private final static int REQUEST_ADD=1; 
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
@@ -33,11 +35,28 @@ public class ListaRelacionActivity extends Activity {
 		case R.id.menuAgregar:
 			//Aqui llamo a la actividad
 			Intent intent=new Intent(this,Ejercicio1Activity.class);
-			startActivity(intent);
+			startActivityForResult(intent,REQUEST_ADD);
 			break;
 			
 		}
 		
 		return super.onOptionsItemSelected(item);
 	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data){
+		
+		if(resultCode==Activity.RESULT_OK){
+			
+			switch(requestCode){
+			case REQUEST_ADD:
+				//Aqui deberia agregar elementos a la lista
+				Log.d("Ejercicio1","Requested Add");
+				break;
+			}
+			
+		}
+		
+	}
+	
 }
